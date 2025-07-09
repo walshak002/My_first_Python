bfc1095e6402868f69f8e02067965a78f2579ac4
walshak123 <patrickwalshak1@gmail.com>
2025-07-09 15:54:38 +0100

    new task

--- wednesday.py
+++ wednesday.py
@@ -1,11 +1,24 @@
 #secret_word = "PythonIsAmazing"
 #print(secret_word[0:4:2])
-
+'''
 first_name = "python"
 first_name = first_name.capitalize()
 print(first_name)
 print(first_name[0:].capitalize())
 '''
+'''
 word = "python is amazing"
 print(word.capitalize())
 '''
+
+#Task_one:
+email = "user@domian.com"
+add = "gmail.com"
+mail = (email[:5] + add[:9])
+print(mail)
+
+word = "good"
+word_length = len(word) //2
+first = word[: word_length]
+second = word[word_length :]
+print(first, second)
